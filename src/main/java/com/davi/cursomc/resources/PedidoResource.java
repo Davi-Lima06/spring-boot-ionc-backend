b6bb27aa54f19ea9fd783da9200ca5b3f8b8f6b2
Davi Lima <davips22012@gmail.com>
2021-07-16 13:44:46 -0300

    Endpoint /pedidos/id implementado

---
+++ src/main/java/com/davi/cursomc/resources/PedidoResource.java
@@ -0,0 +1,28 @@
+package com.davi.cursomc.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.davi.cursomc.domain.Pedido;
+import com.davi.cursomc.services.PedidoService;
+
+@RestController
+@RequestMapping(value="/pedidos")
+public class PedidoResource {
+	
+	@Autowired
+	private PedidoService service;
+
+	@RequestMapping(value = "{id}",method = RequestMethod.GET )
+	public ResponseEntity<?> find(@PathVariable Integer id) { // O RESPONSEENTITY É UM TIPO ESPECIAL DO SPRING QUE ELE JÁ ARMAZENA VARIAS INFORMAÇÕES
+															  // DE UMA RESPOSTA HTTP PARA UM SERVIÇO REST
+		
+		Pedido obj = service.buscar(id);
+		
+		return ResponseEntity.ok().body(obj) ;
+	}
+}
---
+++ src/main/java/com/davi/cursomc/services/PedidoService.java
@@ -0,0 +1,27 @@
+package com.davi.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.davi.cursomc.domain.Pedido;
+import com.davi.cursomc.repositories.PedidoRepository;
+import com.davi.cursomc.services.exceptions.ObjectNotFoundException;
+
+
+
+@Service
+public class PedidoService {  // ESSA CLASSE SERVE PARA OPERAÇÕES E CONSULTAS
+	
+	@Autowired // ESSA DEPENDENCIA VAI SER INSTANCIADA AUTOMATICAMENTE PELO SPRING
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) throws ObjectNotFoundException {
+		
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+		
+	}
+
+}
